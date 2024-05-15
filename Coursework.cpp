bcf3595b3d78811750689da5a21a9f59ac083380
Dmitriy Alexeev <dmitriylenovo328@gmail.com>
2024-05-15 20:58:29 +0300

    Классы готовы к работе

--- Coursework.cpp
+++ Coursework.cpp
@@ -1,7 +1,145 @@
 ﻿#include "Header.h"
 
+static int Menu();
+
+static int SubMenu();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(0, "ru");
+	
+	/*Date prihod;
+
+	cin >> prihod;*/
+	
+	//Ecard ebook;
+
+	/// Указатель для добавления объектов классов
+	//Product* universal = nullptr;
+
+	short choise = 0; // для выбора класса
+	int var = 0; // для выбора действия
+	
+	int all_cnt = 0;
+
+	int avto_count = 0;
+	Car avto[MAX];
+
+	int food_count = 0;
+	Foodstuff food[MAX];
+
+	int furniture_cnt = 0;
+	Furniture furniture[MAX];
+
+	while (true)
+	{
+		
+		choise = Menu();
+
+		
+		if (choise == 1)
+		{
+			var = SubMenu();
+			if (var == 1)
+			{
+				Car car_temp;
+
+				cin >> car_temp;
+					
+				avto[avto_count++] = car_temp;
+			}
+			else if (var == 2)
+			{
+				/// При первом проходе сюда не заходить
+				for (size_t i = 0; i < avto_count; i++)
+				{
+					cout << avto[i] << endl;
+				}
+			}
+			else if (var == 3)
+			{
+				break;
+			}
+		}
+		else if (choise == 2)
+		{
+			var = SubMenu();
+			if (var == 1)
+			{
+				Foodstuff food_temp;
+
+				cin >> food_temp;
+
+				food[food_count++] = food_temp;
+			}
+			else if (var == 2)
+			{
+				/// функция для вывода массива элементов класса Продукты
+			}
+			else if (var == 3)
+			{
+				break;
+			}
+		}
+		else if (choise == 3)
+		{
+			var = SubMenu();
+			if (var == 1)
+			{
+				Furniture furniture_temp;
+
+				cin >> furniture_temp;
+
+				furniture[furniture_cnt++] = furniture_temp;
+			}
+			else if (var == 2)
+			{
+				/// функция для вывода массива элементов класса Мебель
+			}
+		}
+		else
+		{
+			exit(3);
+		}
+		
+		all_cnt++;
+
+		system("cls");
+	}
+	
+	
+
+	return 0;
+}
+
+
+static int Menu()
+{
+	int choise;
+	cout << setw(20) << left << "Автомобиль" << " ( 1 ) " << endl;
+	cout << setw(20) << left << "Продукт " << " ( 2 )" << endl;
+	cout << setw(20) << left << "Мебель " << " ( 3 )" << endl;
+	cout << setw(20) << left << "Завершить работу " << " ( 0 )" << endl;
+	cin >> choise;
+	while (choise < 0 || choise > 3)
+	{
+		cout << "Неверный ввод значения\n Введите заново" << endl;
+		cin >> choise;
+	}
+	return choise;
+}
+
+static int SubMenu()
+{
+	int choise;
+	cout << setw(20) << left << "Добавить информацию" << "( 1 )" << endl;
+	cout << setw(20) << left << "Каталог" << "( 2 )" << endl;
+	cout << setw(20) << left << "Назад" << "( 0 )" << endl;
+	cin >> choise;
+	while (choise < 0 || choise > 2)
+	{
+		cout << "Неверный ввод значения\n Введите заново" << endl;
+		cin >> choise;
+	}
+	return choise;
 }
--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,29 @@
 #include "Header.h"
 
+
+//#define DEBUGG
+
+Date::Date()
+{
+#ifdef DEBUGG
+	cout << "Конструктор класса Date " << this << ' ' << __FILE__ << ' ' << __LINE__ << endl;
+#endif // DEBUGG
+	dd = 0;
+	mm = 0;
+	gg = 0;
+}
+
+Date::Date(int d, int m, int g)
+{
+	this->dd = d;
+	this->mm = m;
+	this->gg = g;
+}
+
+Date::~Date()
+{
+}
+
 void Product::SetPrice(double price)
 {
 	this->price = price;
@@ -15,40 +39,254 @@ double Product::GetPrice() const
 	return this->price;
 }
 
-double Product::GetCnt() const
+int Product::GetCnt() const
 {
 	return this->count;
 }
 
-ostream& operator<<(ostream& out, const Date& obj)
-{
-	out << obj.dd << " : " << obj.mm << " : " << obj.gg << '\t';
-	return out;
-}
-
 istream& operator>>(istream& in, Date& obj)
 {
+	cout << "Введите дату (формат дд:мм:гггг)\t";
 	in >> obj.dd >> obj.mm >> obj.gg;
 	return in;
 }
 
-void Foodstuff::SetManufacture(Date& obj)
+ostream& operator<<(ostream& out, const Date& obj)
 {
-	this->Manufacture = obj;
+	out.fill('0');
+	out <<  setw(2) << obj.dd << '.' << 
+			setw(2) << obj.mm << '.' << 
+			setw(4) << obj.gg << '\t';
+	return out;
 }
 
-void Foodstuff::SetExpiration(Date& obj)
+istream& operator>>(istream& in, Foodstuff& obj)
 {
-	this->Expiration = obj;
+	cout << "Введите категорию товара(0 - рыба, 1 - мясо, 2 - молоко, 3 - хлебобулочные изделия";
+	in >> obj.num_categ;
+
+	cout << "Введите срок годности (укажите дни)";
+	in >> obj.expiration_date;
+
+	cout << "Введите дату поступления";
+	in >> obj.Delivery;
+
+	cout << "Введите дату продажи";
+	in >> obj.Sale;
+
+	cout << "Введите цену товара";
+	in >> obj.price;
+
+	cout << "Введите количество товара";
+	in >> obj.count;
+
+	return in;
 }
 
-Date Foodstuff::GetManufacture()
+ostream& operator<<(ostream& out, const Foodstuff& obj)
 {
-	return this->Manufacture;
+	switch (obj.num_categ)
+	{
+	case obj.fish:
+		out << "Категория товара : морепродукты";
+		break;
+	case obj.meat:
+		out << "Категория товара : мясо";
+		break;
+	case obj.milk:
+		out << "Категория товара : молоко";
+		break;
+	case obj.bread:
+		out << "Категория товара : хлебобулочные изделия";
+		break;
+
+	default:
+		out << "Пощёл нахуййййййй!!!!!!!!!!";
+		break;
+	}
+	out << "Срок годности товара " << obj.expiration_date << endl;
+	out << "Цена товара " << obj.price << endl;
+	out << "Количество товара " << obj.count << endl;
+	out << "Дата поступления" << obj.Delivery << endl;
+	out << "Дата продажи" << obj.Sale << endl;
+	return out;
 }
 
-Date Foodstuff::GetExpiration()
+istream& operator>>(istream& in, Furniture& obj)
 {
-	return this->Expiration;
+	cout << "Введите марку мебели";
+	in >> obj.brand;
+
+	cout << "Введите модель мебели";
+	in >> obj.name;
+
+	cout << "Введите дату поступления";
+	in >> obj.Delivery;
+
+	cout << "Введите дату продажи";
+	in >> obj.Sale;
+
+	cout << "Введите цену мебели";
+	in >> obj.price;
+
+	cout << "Введите количество мебели";
+	in >> obj.count;
+
+	return in;
 }
+
+ostream& operator<<(ostream& out, const Furniture& obj)
+{
+	out << "Марка мебели " << obj.brand << endl;
+	out << "Модель мебели " << obj.name << endl;
+	out << "Цена мебели " << obj.price << endl;
+	out << "Количество мебели " << obj.count << endl;
+	out << "Дата поступления" << obj.Delivery << endl;
+	out << "Дата продажи" << obj.Sale << endl;
+	return out;
+}
+
+istream& operator>>(istream& in, Car& obj)
+{
+	cout << "Введите марку автомобиля";
+	in >> obj.brand;
+
+	cout << "Введите модель автомобиля";
+	in >> obj.model;
+
+	cout << "Введите дату поступления";
+	in >> obj.Delivery;
+
+	cout << "Введите дату продажи";
+	in >> obj.Sale;
+
+	cout << "Введите цену автомобиля";
+	in >> obj.price;
+
+	cout << "Введите количество автомобилей";
+	in >> obj.count;
+
+	return in;
+
+}
+
+ostream& operator<<(ostream& out, const Car& obj)
+{
+	out << "Марка автомобиля " << obj.brand << endl;
+	out << "Модель автомобиля " << obj.model << endl;
+	out << "Цена автомобиля " << obj.price << endl;
+	out << "Количество автомобилей " << obj.count << endl;
+	out << "Дата поступления" << obj.Delivery << endl;
+	out << "Дата продажи" << obj.Sale << endl;
+	return out;
+}
+
+Foodstuff::Foodstuff()
+{
+#ifdef DEBUGG
+	cout << "Конструктор класса Foodstuff " << this << ' ' << __FILE__ << ' ' << __LINE__ << endl;
+#endif // DEBUGG
+	this->expiration_date = 0;
+}
+
+Foodstuff::~Foodstuff()
+{
+}
+
+//void Foodstuff::SetManufacture(Date& obj)
+//{
+//	this->Manufacture = obj;
+//}
+//
+//void Foodstuff::SetExpiration(Date& obj)
+//{
+//	this->Expiration = obj;
+//}
+//
+//Date Foodstuff::GetManufacture() const
+//{
+//	return Manufacture;
+//}
+//
+//Date Foodstuff::GetExpiration() const
+//{
+//	return this->Expiration;
+//}
+
+string Foodstuff::Accesories()
+{
+	return " класс Еда ";
+}
+
+void Product::SetDelivery(Date& obj)
+{
+	this->Delivery = obj;
+}
+
+void Product::SetSale(Date& obj)
+{
+	this->Sale = obj;
+}
+
+Date Product::GetDelivery() const
+{
+	return this->Delivery;
+}
+
+Date Product::GetSale() const
+{
+	return this->Sale;
+}
+
+Furniture::Furniture()
+{
+#ifdef DEBUGG
+	cout << "Конструктор класса Furniture " << this << ' ' << __FILE__ << ' ' << __LINE__ << endl;
+#endif // DEBUGG
+	this->count = 0;
+	this->price = 0;
+}
+
+Furniture::~Furniture()
+{
+
+}
+
+string Furniture::Accesories()
+{
+	return " класс Мебель ";
+}
+
+Car::Car()
+{
+#ifdef DEBUGG
+	cout << "Конструктор класса Car " << this << ' ' << __FILE__ << ' ' << __LINE__ << endl;
+#endif // DEBUGG
+}
+
+Car::Car(string b, string m) : brand(b), model(m)
+{
+}
+
+Car::~Car()
+{
+}
+
+string Car::Accesories()
+{
+	return " класс Машина ";
+}
+
+//Ecard::Ecard()
+//{
+//#ifdef DEBUGG
+//	cout << "Конструктор класса Ecard " << this << ' ' << __FILE__ << ' ' << __LINE__ << endl;
+//#endif // DEBUGG
+//
+//}
+//
+//Ecard::~Ecard()
+//{
+//
+//}
 
